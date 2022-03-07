8de12d0783ae8ac0aa4af9c378b25028273a5d75
sobaryton <solene.bary@gmail.com>
2022-03-07 17:22:57 +0000

    installing mui

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+    <div>
+      Hello baby 
     </div>
   );
 }
