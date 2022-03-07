1816488d9869dd22f731c6a68ff8ed873d922d62
sobaryton <solene.bary@gmail.com>
2022-03-07 18:17:30 +0000

    playing with card

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import * as React from 'react';
+import { default as BasicCard } from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import CardMedia from '@mui/material/CardMedia/CardMedia';
 
 const Card = () => {
-  const foo = 'bar'
   return (
-    <>
-      {foo}
-    </>
+    <BasicCard sx={{ minWidth: 275, maxWidth: 345 }} variant="outlined">
+      <CardMedia
+        component="img"
+        height="140"
+        image="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2"
+        alt="item"
+      />
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          Word of the Day
+        </Typography>
+        <Typography variant="h5" component="div">
+          blabla
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          adjective
+        </Typography>
+        <Typography variant="body2">
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </BasicCard>
   )
 }
 
